8f71ed9a5e99bf86c5fe5e9703124dd079e863cd
Moibe <moi_estrello@hotmail.com>
2022-03-17 22:30:32 -0600

    Second Piñata Test
    Leave only piñata code.

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 61824;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from Moibe Deploybot Done Correctly! Node.js\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 const axios = require('axios');
 const testAuthentication = () => {
     const url = `https://api.pinata.cloud/data/testAuthentication`;

02b4b8c6528a9f9eb9bf46dc442012ace4cff215
Moibe <moi_estrello@hotmail.com>
2022-03-17 23:23:53 -0600

    Back to Normal

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const axios = require('axios');
-const testAuthentication = () => {
-    const url = `https://api.pinata.cloud/data/testAuthentication`;
-    return axios
-        .get(url, {
-            headers: {
-                pinata_api_key: 'c56ebc06628414410e5f',
-                pinata_secret_api_key: '505b93f46c2bc646801d6de0df124e52c3621eab432b106546ec5ea130fc57c2'
-            }
-        })
-        .then(function (response) {
-            console.log(response);
-            alert("Mint Done"); 
-        })
-        .catch(function (error) {
-            console.log(response);
-            alert("Not Done"); 
-        });
-};
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 61824;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World from Moibe Deploybot Done Correctly! Node.js\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
